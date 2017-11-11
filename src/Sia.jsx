7c0fc2520f78dac9229ec28cacbe9fa612ff5e10
Hao Liu <haol@yahoo-inc.com>
2017-11-10 17:02:57 -0800

    some experiments

--- src/Sia.jsx
+++ src/Sia.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import {Editor, EditorState, RichUtils, CompositeDecorator, Modifier} from 'draft-js';
+import {Editor, EditorState, RichUtils, CompositeDecorator, Modifier, SelectionState, ContentState} from 'draft-js';
 
 const BOLD_REGEX = /\*\*([\w]+)\*\*/g;
+const HEADING_REGEX = /# (.+)$/g;
 
 function findWithRegex(regex, contentBlock, callback) {
   const text = contentBlock.getText();
   let matchArr, start, end;
   while ((matchArr = regex.exec(text)) !== null) {
     start = matchArr.index;
-    end = start + matchArr[0].length
+    end = start + matchArr[0].length;
     callback(start, end);
   }
 }
@@ -17,8 +18,24 @@ function boldStrategy(contentBlock, callback, contentState) {
   findWithRegex(BOLD_REGEX, contentBlock, callback);
 }
 
+function headingStrategy(contentBlock, callback, contentState) {
+  findWithRegex(HEADING_REGEX, contentBlock, callback);
+}
+
 const boldSpan = props => {
-  let editorState = props.parent.state.editorState;
+  // const editor = props.parent.editor;
+
+  const { editorState } = props.parent.state;
+
+  // const { decoratedText } = props;
+  // const { start, offsetKey } = props.children[0].props;
+  // console.log(props.children[0].props, start, offsetKey);
+
+  // const selectionState = SelectionState.createEmpty(offsetKey).merge({
+  //   anchorOffset: start,
+  //   focusOffset: start + decoratedText.length,
+  // });
+
   // let newState = Modifier.replaceText(
   //   editorState.getCurrentContent(),
   //   selectionState,
@@ -26,10 +43,21 @@ const boldSpan = props => {
   //   null,
   //   entityKey,
   // );
+  
+  // props.parent.setState({state: EditorState.push(
+  //   editorState, 
+  //   ContentState.createFromText('123'), 
+  //   'insert-characters'
+  // )});
+  console.log('bold', props);
 
   return <span className="Sia-bold">{props.children}</span>;
 };
 
+const headingSpan = props => {
+  console.log('heading', props);
+  return <span className="Sia-heading">{props.children}</span>;
+}
 
 
 class Sia extends React.Component {
@@ -40,6 +68,10 @@ class Sia extends React.Component {
       strategy: boldStrategy,
       component: boldSpan,
       props: {parent: this}
+    }, {
+      strategy: headingStrategy,
+      component: headingSpan,
+      props: {parent: this}
     }]);
 
     this.state = {editorState: EditorState.createEmpty(compositeDecorator)};
@@ -49,6 +81,7 @@ class Sia extends React.Component {
     return (
       <div className="Sia">
         <Editor
+          ref={editor => this.editor = editor}
           editorState={this.state.editorState}
           onChange={this.onChange.bind(this)}
           handleKeyCommand={this.handleKeyCommand.bind(this)}
@@ -58,11 +91,11 @@ class Sia extends React.Component {
   }
   onChange(editorState) {
     const contentState = editorState.getCurrentContent();
-    console.log(contentState);
+    // console.log(contentState);
     this.setState({editorState});
   }
   handleKeyCommand(command, editorState) {
-    console.log(command);
+    // console.log(command);
     const newState = RichUtils.handleKeyCommand(editorState, command);
     if (newState) {
       this.onChange.call(this, newState);
