488beedcbb87d54883beb41e6fb77c987de673db
fuermosi777 <liuhao1990@gmail.com>
2017-11-11 18:54:09 -0800

    make hader 1 working

--- src/Sia.jsx
+++ src/Sia.jsx
@@ -1,80 +1,35 @@
 import React from 'react';
 import {Editor, EditorState, RichUtils, CompositeDecorator, Modifier, SelectionState, ContentState} from 'draft-js';
 
-const BOLD_REGEX = /\*\*([\w]+)\*\*/g;
-const HEADING_REGEX = /# (.+)$/g;
-
-function findWithRegex(regex, contentBlock, callback) {
-  const text = contentBlock.getText();
-  let matchArr, start, end;
-  while ((matchArr = regex.exec(text)) !== null) {
-    start = matchArr.index;
-    end = start + matchArr[0].length;
-    callback(start, end);
-  }
-}
-
-function boldStrategy(contentBlock, callback, contentState) {
-  findWithRegex(BOLD_REGEX, contentBlock, callback);
-}
-
-function headingStrategy(contentBlock, callback, contentState) {
-  findWithRegex(HEADING_REGEX, contentBlock, callback);
-}
-
-const boldSpan = props => {
-  // const editor = props.parent.editor;
-
-  const { editorState } = props.parent.state;
-
-  // const { decoratedText } = props;
-  // const { start, offsetKey } = props.children[0].props;
-  // console.log(props.children[0].props, start, offsetKey);
-
-  // const selectionState = SelectionState.createEmpty(offsetKey).merge({
-  //   anchorOffset: start,
-  //   focusOffset: start + decoratedText.length,
-  // });
-
-  // let newState = Modifier.replaceText(
-  //   editorState.getCurrentContent(),
-  //   selectionState,
-  //   "my new text",
-  //   null,
-  //   entityKey,
-  // );
-  
-  // props.parent.setState({state: EditorState.push(
-  //   editorState, 
-  //   ContentState.createFromText('123'), 
-  //   'insert-characters'
-  // )});
-  console.log('bold', props);
-
-  return <span className="Sia-bold">{props.children}</span>;
+const changeCurrentBlockType = (
+  editorState,
+  type,
+  text,
+  blockMetadata = {}
+) => {
+  const currentContent = editorState.getCurrentContent();
+  const selection = editorState.getSelection();
+  const key = selection.getStartKey();
+  const blockMap = currentContent.getBlockMap();
+  const block = blockMap.get(key);
+  const data = block.getData().merge(blockMetadata);
+  const newBlock = block.merge({ type, data, text: text || "" });
+  const newSelection = selection.merge({
+    anchorOffset: 0,
+    focusOffset: 0,
+  });
+  const newContentState = currentContent.merge({
+    blockMap: blockMap.set(key, newBlock),
+    selectionAfter: newSelection,
+  });
+  return EditorState.push(editorState, newContentState, "change-block-type");
 };
-
-const headingSpan = props => {
-  console.log('heading', props);
-  return <span className="Sia-heading">{props.children}</span>;
-}
-
 
 class Sia extends React.Component {
   constructor(props) {
     super(props);
 
-    const compositeDecorator = new CompositeDecorator([{
-      strategy: boldStrategy,
-      component: boldSpan,
-      props: {parent: this}
-    }, {
-      strategy: headingStrategy,
-      component: headingSpan,
-      props: {parent: this}
-    }]);
-
-    this.state = {editorState: EditorState.createEmpty(compositeDecorator)};
+    this.state = {editorState: EditorState.createWithContent(ContentState.createFromText(`123\n**123**`))};
 
   }
   render() {
@@ -84,24 +39,66 @@ class Sia extends React.Component {
           ref={editor => this.editor = editor}
           editorState={this.state.editorState}
           onChange={this.onChange.bind(this)}
+          blockRendererFn={this.blockRendererFn.bind(this)}
           handleKeyCommand={this.handleKeyCommand.bind(this)}
+          handleBeforeInput={this.handleBeforeInput.bind(this)}
+          onFocus={this.handleFocus.bind(this)}
         />
       </div>
     );
   }
   onChange(editorState) {
     const contentState = editorState.getCurrentContent();
-    // console.log(contentState);
+    // console.log('change', contentState);
     this.setState({editorState});
   }
   handleKeyCommand(command, editorState) {
-    // console.log(command);
+    // console.log('key', command);
     const newState = RichUtils.handleKeyCommand(editorState, command);
     if (newState) {
       this.onChange.call(this, newState);
       return 'handled';
     }
     return 'not-handled';
+  }
+  handleBeforeInput(character: string, editorState: EditorState) {
+    // console.log('beforeInput', character);
+
+    let newEditorState;
+    
+    const currentSelection = editorState.getSelection();
+    const key = currentSelection.getStartKey();
+    const text = editorState
+      .getCurrentContent()
+      .getBlockForKey(key)
+      .getText();
+  
+    const position = currentSelection.getAnchorOffset();
+    const line = [text.slice(0, position), character, text.slice(position)].join('');
+    const blockType = RichUtils.getCurrentBlockType(editorState);
+
+    if (line.indexOf(`# `) === 0) {
+      newEditorState = changeCurrentBlockType(
+        editorState,
+        'header-one',
+        line.replace(/^#+\s/, "")
+      );
+
+      this.setState({editorState: newEditorState});
+      return 'handled';
+    }
+    
+
+    console.log(text, line, blockType);
+
+    return 'not-handled';
+  }
+  handleFocus() {
+    // console.log('focus')
+  }
+  blockRendererFn(contentBlock) {
+    // console.log(contentBlock.getType())
+
   }
 }
 
